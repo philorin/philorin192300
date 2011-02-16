5dcfc587b41a31908314a73b7e29240b99748aa5
porin <porin@porin-dev.(none)>
2011-02-16 13:35:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Philorin192300::Application.config.secret_token = '134791e9672bbcf122a68fc51e261ff770ac1d33ae74b2a2ae98e24dd4030a751bf093747eb10c632bbd3168a9526d2d45bdc915bcd87ed8ed531927307ef6f5'
